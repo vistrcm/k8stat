c7b7f7bdf2996ec78eed7b997c0ec15234154cfc
Stanislav Vitko <stas@vitko.me>
2022-11-06 19:08:16 -0800

    feat: some retries

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"log"
+	"math"
+	"math/rand"
 	"time"
 )
 
@@ -39,11 +41,32 @@ func main() {
 func loop(ctx context.Context, clientset *kubernetes.Clientset, statmap *StatMap) {
 	ticker := time.Tick(LoopInterval)
 	for ; true; <-ticker { // interesting hack to start right away
-		if err := loopStep(ctx, clientset, statmap); err != nil {
+		if err := loopStepWithRetry(ctx, clientset, statmap); err != nil {
 			panic(err.Error())
 		}
 		statmap.Print()
 	}
+}
+
+// retry loopStep 3 times if errors. Return last error.
+// log other errors.
+func loopStepWithRetry(ctx context.Context, clientset *kubernetes.Clientset, statmap *StatMap) error {
+	retries := 3
+	var err error
+	for i := 0; i < retries; i++ {
+		if err = loopStep(ctx, clientset, statmap); err != nil {
+			delay := getDelay(i)
+			log.Printf("ERROR: can't get data %v. Try %d/%d. Delay: %s\n", err, i+1, retries, delay)
+			time.Sleep(delay)
+		}
+	}
+	return err
+}
+
+func getDelay(i int) time.Duration {
+	delay := math.Pow(2, float64(i))
+	jitter := rand.Float64() * 2
+	return time.Duration(delay+jitter) * time.Second
 }
 
 func loopStep(ctx context.Context, clientset *kubernetes.Clientset, statmap *StatMap) error {
